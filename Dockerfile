2f0863ff1e595a00bcc4bca24055834f3ef2516f
Nat Tabris <46289310+ntabris@users.noreply.github.com>
2019-10-14 09:32:02 -0400

    Initial version of files.

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM conda/miniconda3 as meshparty
+
+RUN apt-get update && apt-get install -y gcc mayavi2
+RUN conda install -c conda-forge pyembree pykdtree python-blosc
+RUN pip install vtk mayavi annotationframeworkclient meshparty
+
+FROM meshparty
+
+ADD *.json /root/.cloudvolume/secrets/
+COPY meshdown.py meshdown.py
---
+++ meshdown.py
@@ -0,0 +1,16 @@
+import sys
+from meshparty import trimesh_io
+
+meshlist = sys.argv[1]
+meshlist = list(map(int, meshlist.split(",")))
+
+with open("datasource.txt", "r") as f:
+    datasource = f.read()
+
+save_dir = "meshes"
+
+trimesh_io.download_meshes(
+        meshlist,
+        save_dir,
+        datasource,
+        fmt="obj")
